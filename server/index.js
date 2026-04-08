c616c91307c3d904ca45dd3a7de6c22b1f2b03de
bhavesh malviya <bhaveshmalviya@bhaveshs-MacBook-Pro.local>
2026-04-09 02:06:13 +0530

    Added root route

--- server/index.js
+++ server/index.js
@@ -10,6 +10,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("🚀 ShareBite Backend is Running");
+});
+
 // Routes
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/donations', require('./routes/donations'));
