9525609f8ef2e87aa3118ff0167814980fa2d01e
bhavesh malviya <bhaveshmalviya@bhaveshs-MacBook-Pro.local>
2026-09-24 00:36:31 +0530

    Fix certificate upload path

--- server/routes/upload.js
+++ server/routes/upload.js
@@ -1,42 +1,39 @@
 const express = require('express');
 const router = express.Router();
 const multer = require('multer');
+const path = require('path');
 
 const storage = multer.diskStorage({
 
- destination: function(req, file, cb) {
+  destination: function(req, file, cb) {
+    cb(null, path.join(__dirname, '../uploads'));
+  },
 
-    cb(null, '/tmp');
-
- },
-
- filename: function(req, file, cb) {
-
-   cb(
-     null,
-     Date.now() + '-' + file.originalname
-   );
-
- }
+  filename: function(req, file, cb) {
+    cb(
+      null,
+      Date.now() + '-' + file.originalname
+    );
+  }
 
 });
 
 const upload = multer({
- storage: storage
+  storage: storage
 });
 
 router.post(
- '/certificate',
- upload.single('certificate'),
- (req, res) => {
+  '/certificate',
+  upload.single('certificate'),
+  (req, res) => {
 
-   console.log(req.file);
+    console.log(req.file);
 
-   res.json({
-     filePath: req.file.path
-   });
+    res.json({
+      filePath: `/uploads/${req.file.filename}`
+    });
 
- }
+  }
 );
 
 module.exports = router;
